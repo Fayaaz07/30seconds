b1c6dc44d5ba6cf8da844a92c586c6e04257a07a
Peter van der Net <petervandernet@gmail.com>
2019-12-10 11:40:59 +0100

    Added base data

--- resources/js/components/Game.vue
+++ resources/js/components/Game.vue
@@ -11,16 +11,51 @@
                         I'm an example parent component.
                     </div>
                 </div>
-                <game-child></game-child>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Parent component mounted.')
+export default {
+    data() {
+        return {
+            teams: {
+                team1: {
+                    name: "Team 1",
+                    currentPoints: 0,
+                },
+                team2: {
+                    name: "team 2",
+                    currentPoints: 0,
+                },
+            },
+            pointsToWin: 30, //One right answer = one point
+            timePerRound: 30, //Time in seconds
+            turns: 1,
         }
-    }
+    },
+    methods: {
+        updatePoints(team, points) {
+            if(this.teams.team1.name === team.name) {
+                this.teams.team1.currentPoints += points;
+                console.log('Team 1 points: ' + this.teams.team1.currentPoints);
+            } else {
+                this.teams.team2.currentPoints += points;
+                console.log('Team 2 points: ' + this.teams.team2.currentPoints);
+            }
+            this.checkPoints();
+        },
+        checkPoints() {
+            if(this.teams.team1.currentPoints >= this.pointsToWin || this.teams.team2.currentPoints >= this.pointsToWin) {
+                this.endGame();
+            } else {
+                console.log('Game is not over yet');
+            }
+        },
+        endGame() {
+            console.log('Game has ended');
+        }
+    },
+}
 </script>
